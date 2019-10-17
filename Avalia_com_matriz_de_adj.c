6e31f454763a1f4fcb5507ea7f18df02f3f0a4e9
SamuelHericles <samuel.herilces@alu.ufc.br>
2019-10-17 06:49:46 -0300

    Adicionando os ultimos acabamentos no projeto

--- Avalia_com_matriz_de_adj.c
+++ Avalia_com_matriz_de_adj.c
@@ -19,9 +19,9 @@ void setEdge(int valor, int i, int j, grafo g){
 
 int getIndex(int i, int j, int n){
         if(i<j)
-            return (2*n-i-1)*i/2+(j-1)-1;
+            return (2*n-i-1)*i/2+(j-i)-1;
         else
-            return (2*n-j-1)*j/2+(i-1)-1;
+            return (2*n-j-1)*j/2+(i-j)-1;
 }
 
 void mostraGrafo(grafo g){
@@ -36,6 +36,15 @@ void mostraGrafo(grafo g){
     printf("\n");
 }
 
+void mostra_dois_grafo(grafo g1,grafo g2){
+
+    printf(" Grafo 1\n");
+    mostraGrafo(g1);
+    printf(" Grafo 2\n");
+    mostraGrafo(g2);
+
+}
+
 grafo criaGrafo(int n,int p){
     grafo g = malloc(sizeof(grafo));
     g->tamanho = n;
@@ -43,15 +52,64 @@ grafo criaGrafo(int n,int p){
 
     for(int i = 0; i < (g->tamanho*(g->tamanho-1)/2) ; i++){
         int prob = rand()%100;
-        if(prob>p) g->adjMatrix[i] = 1;
+        if(prob < p) g->adjMatrix[i] = 1;
         else g->adjMatrix[i] = 0;
     }
     printf("\n");
     return g;
 }
 
+////////////////////////////////////////////////////////////////////////
+
+void BP_visit(grafo g,int *vertice, int i){ 
+    vertice[i] = -1;
+    for(int j = 0; j < g->tamanho; j++){
+        if(edge(i,j,g) == 1){
+            if(vertice[j] == 0){
+                BP_visit(g,vertice,j);
+            }
+        }
+    }
+    vertice[i] = 1;
+}
+
+int Busca_em_profundidade(grafo g){ 
+   //-1 cinza
+   // 0 branco
+   // 1 preto
+   int a = 0;
+   int vertices[g->tamanho];
+
+   for(int i = 0; i < g->tamanho; i++) 
+       vertices[i] = 0;
+
+   for(int i = 0; i < g->tamanho; i++) {
+       if(vertices[i] == 0){
+           a++;
+           BP_visit(g,vertices,i);
+       }
+       /*
+        printf("\n%d entrada(s) no BP_visit\n",a);
+        printf("\nVertices->");
+        for(int i = 0; i < g->tamanho ; i++)
+                printf(" |%2d | ",i+1);
+        printf("\n");
+            printf("          ");
+        for(int i = 0; i < g->tamanho ; i++)
+                printf(" |%2d | ",vertices[i]);
+        printf("\n\n");
+        */
+
+    }
+
+    return a;
+    
+}
+
+////////////////////////////////////////////////////////////////////////
+
 int Verifica_num_vertices(grafo g1, grafo g2){
-    printf("%d||%d",g1->tamanho,g2->tamanho);
+    //printf("\n%d||%d",g1->tamanho,g2->tamanho);
     if(g1->tamanho == g2->tamanho)
         return 1;
     return 0;
@@ -73,99 +131,149 @@ int Verifica_num_arestas(grafo g1, grafo g2){
             }
         }
     }
-    printf("%d||%d",a,b);
+    //printf("\n%d||%d",a,b);
     if(a == b)
         return 1;
     return 0;
 
 }
 
-void qtdVerticeMesmoGrau(grafo g1, grafo g2){
+void Verifica_vertices_de_mesmo_grau(grafo g1, grafo g2){
  int *a1, *a2, i, j,aux;
  a1 = (int*)malloc(g1->tamanho*sizeof(int));
  a2 = (int*)malloc(g2->tamanho*sizeof(int)); 
  
- for(i=0;i<g1->tamanho;i++){
-  a1[i]=0;
- }
- for(i=0;i<g2->tamanho;i++){
-  a2[i]=0;
- }
- 
- for(i = 0; i< g1->tamanho;i++){
-  aux = 0;
+    for(i=0;i<g1->tamanho;i++){
+         a1[i]=0;
+    }
+    for(i=0;i<g2->tamanho;i++){
+        a2[i]=0;
+    }
+    
+    for(i = 0; i< g1->tamanho;i++){
+         aux = 0;
         for(j = 0; j< g1->tamanho;j++){
-         if(i!=j)
-    aux+=edge(i,j,g1);
-     }
-     a1[aux]+=1;
+            if(i!=j)  aux+=edge(i,j,g1);
+        }
+        a1[aux]+=1;
+        }
+    
+    for(i = 0; i< g2->tamanho;i++){
+    aux = 0;
+            for(j = 0; j< g2->tamanho;j++){
+            if(i!=j)
+        aux+=edge(i,j,g2);
+        }
+        a2[aux]++;
+        }
+    for(i=0;i<g2->tamanho;i++){
+        printf("%d    %d",a1[i],a2[i]);
     }
- 
- for(i = 0; i< g2->tamanho;i++){
-  aux = 0;
-        for(j = 0; j< g2->tamanho;j++){
-         if(i!=j)
-    aux+=edge(i,j,g2);
-     }
-     a2[aux]++;
-    }
- for(i=0;i<g2->tamanho;i++){
-  printf("%d    %d\n",a1[i],a2[i]);
- }
 }
 
+int Verifica_desconexo(grafo g1, grafo g2){
 
-////////////////////////////////////////////////////////////////////////
-
-int BP_visit(grafo g, int i){ 
-   for(i = 0; i < g->tamanho; i++){
-       for(int j = i+1; j < g->tamanho; j++){
-           if(edge(i,j,g) == 0)
-                BP_visit(g,i);
-           return 1;
-       }
-   }
+    int grafo1 = Busca_em_profundidade(g1);
+    int grafo2 = Busca_em_profundidade(g2);
+    //printf("    %d||%d ",grafo1,grafo2);
+    if((grafo1 > 1)&&(grafo2 > 1)){
+       // printf("\nAmbos os grafos sao desconexos\n");
+        return 1;
+    } else {
+        //if(grafo1 > 1) printf("\nGrafo 1 eh desconexo\n");
+        //else if(grafo2 > 1 ) printf("\nGrafo 2 eh desconexo\n");
+        return 0;
+    } 
 }
 
-int Busca_em_profundidade(grafo g){ 
-   int a = 0;
-   int pre[(g->tamanho*(g->tamanho-1)/2)];
-    for(int i = 0; i < (g->tamanho*(g->tamanho-1)/2); i++) 
-        pre[i] = -1;
-    //for(int i = 0; i < (g->tamanho*(g->tamanho-1)/2); i++) {
-    //if(pre[i] == -1)
-    a = BP_visit(g,0); // começa nova etapa
-    //}
-    return a;
+int Verifica_comp_desconexas(grafo g1, grafo g2){
+
+    int grafo1 = Busca_em_profundidade(g1);
+    int grafo2 = Busca_em_profundidade(g2);
+    if(grafo1 == grafo2){
+        //printf("\nAmbos possuem mesma qt de comp desconexas\n");
+        return 1;
+    } else {
+        //printf("\nGrafo 1 -> %d comp desconxa(s)\nGrafo 2 -> %d comp desconxa(s)\n",grafo1,grafo2);
+        return 0;
+    } 
 }
 
-////////////////////////////////////////////////////////////////////////
+int Verifica_isomorfismo(grafo g1, grafo g2){
+    int a = 0, b = 0, c = 0, d = 0 ,e = 0;
 
+    //printf("\n1.Num de vertices-> ");
+    a = Verifica_num_vertices(g1,g2);
+
+    //printf("\n2.Num de arestas -> ");
+    b = Verifica_num_arestas(g1,g2);
+
+    //printf("\n3.Desconexos-> ");
+    c = Verifica_desconexo(g1,g2);
+
+    //printf("\n4.Qt de comp desconexas-> ");
+    d = Verifica_comp_desconexas(g1,g2);
+
+    //printf("\n5.Qtd->\n");
+    //e = Verifica_vertices_de_mesmo_grau(g1,g2);
+
+    if((a+b+c+d+e) == 4)
+        printf(" |%2d| Os grafos sao isomorfos",a+b+c+d+e);
+    else
+        printf(" |%2d| Os grafos NAO sao isomorfos",a+b+c+d+e);
+    
+}
 
 int main(){
     srand(time(NULL));
-
     struct Grafo *g1,*g2;
-    g1 = criaGrafo(5,50);
-    g2 = criaGrafo(5,50);
     
-    printf("\nGrafo 1 ->\t");
-    mostraGrafo(g1);
-    printf("\nGrafo 2 ->\t");
-    mostraGrafo(g2);
+    //Testes
+    
+    //  25%
+    for(int i = 1; i <= 100; i++){
+        g1 = criaGrafo(rand()%15+1,25);
+        g2 = criaGrafo(rand()%15+1,25);
+        printf("%d->",i);
+        Verifica_isomorfismo(g1,g2);
+    }
+    printf("\n");
+    
+    //  50%
+    for(int i = 1; i <= 100; i++){
+        g1 = criaGrafo(rand()%15+1,50);
+        g2 = criaGrafo(rand()%15+1,50);
+        printf("%d->",i);
+        Verifica_isomorfismo(g1,g2);
+    }
     printf("\n");
 
-    printf("Testes>>>\n");
-    printf("Num de vertices->");
-    Verifica_num_vertices(g1,g2);
-    printf("\nNum de arestas->");
-    Verifica_num_arestas(g1,g2);
-    printf("\nQtd->\n");
-    qtdVerticeMesmoGrau(g1,g2);
+    //  75%
+    for(int i = 1; i <= 100; i++){
+        g1 = criaGrafo(rand()%15+1,75);
+        g2 = criaGrafo(rand()%15+1,75);
+        printf("%d->",i);
+        Verifica_isomorfismo(g1,g2);
+    }
+    printf("\n");
 
-    printf("\n\n");
-    //printf("%d\n",Busca_em_profundidade(g1));
+    //  85%
+    for(int i = 1; i <= 100; i++){
+        g1 = criaGrafo(rand()%15+1,85);
+        g2 = criaGrafo(rand()%15+1,85);
+        printf("%d->",i);
+        Verifica_isomorfismo(g1,g2);
+    }
+    printf("\n");
 
-    printf("\n\n");
+    //  95%
+    for(int i = 1; i <= 100; i++){
+        g1 = criaGrafo(rand()%15+1,95);
+        g2 = criaGrafo(rand()%15+1,95);
+        printf("%d->",i);
+        Verifica_isomorfismo(g1,g2);
+    }
+    printf("\n");
+
     
 }
